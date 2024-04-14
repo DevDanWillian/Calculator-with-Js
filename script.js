c9462c0f4f6fe6436467280b597e9771e8e50deb
DevDanWillian <devdanielwillian@gmail.com>
2024-04-14 02:49:40 -0300

    Style Finished

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-window.onload = function() {
-   
-}
+let primeiroNumero = ""
+let primeiroOperando = null
+let operador = null
+restart = false
+
+const igual = document.getElementById("calcBtnIgual")
+const mM = document.getElementById("calcBtnMm")
+const c = document.getElementById("calcBtnC")
+const mais = document.getElementById("calcBtnMais")
+const menos = document.getElementById("calcBtnMenos")
+const vezes = document.getElementById("calcBtnMult")
+const dividir = document.getElementById("calcBtnDiv")
+
