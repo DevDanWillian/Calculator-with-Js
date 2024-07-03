585afee227683c01d98c9eeab82964b87adbe696
Daniel Willian Campos Silva <devdanielwillian@gmail.com>
2024-07-03 13:14:00 -0300

    numbers working

--- script.js
+++ script.js
@@ -1,13 +1,68 @@
-let primeiroNumero = ""
-let primeiroOperando = null
-let operador = null
-restart = false
+const historico = document.getElementById('hist');
 
 const igual = document.getElementById("calcBtnIgual")
-const mM = document.getElementById("calcBtnMm")
+const apaga = document.getElementById("apaga")
 const c = document.getElementById("calcBtnC")
 const mais = document.getElementById("calcBtnMais")
 const menos = document.getElementById("calcBtnMenos")
-const vezes = document.getElementById("calcBtnMult")
 const dividir = document.getElementById("calcBtnDiv")
+const vezes = document.getElementById("calcBtnMult")
 
+
+const screen = document.getElementById("tela")
+
+const calcBtnNum = document.querySelectorAll(".calcBtnNum")
+
+
+//--
+calcBtnNum.forEach(function(btn) {
+   btn.addEventListener("click", function() {
+       // Get the value of the clicked button
+       const value = this.value || this.textContent;
+       
+       // Append the value to the input field
+       screen.value += value;
+   });
+});
+//--
+
+
+igual.addEventListener("click", function() {
+   screen.value = screen;
+   //if (screen.value === "undefined"){
+     // screen.value = ""
+   //}
+});
+
+apaga.addEventListener("click", function() {
+   screen.value = screen.value.substring(0, screen.value.length - 1);
+});
+
+c.addEventListener("click", function() {
+   screen.value=null;
+   historico.value=null;
+})
+
+mais.addEventListener("click", function() {
+   const currentValue = screen.value;
+   historico.value += currentValue + ' +';
+   historico.replace(/[^0-9]/g, '') + currentValue;
+ });
+ 
+ menos.addEventListener('click', function() {
+   const currentValue = screen.value;
+   historico.value += currentValue + ' -';
+   screen.value = '';
+ });
+ 
+ dividir.addEventListener('click', function() {
+   const currentValue = screen.value;
+   historico.value += currentValue + ' /';
+   screen.value = '';
+ });
+ 
+ vezes.addEventListener('click', function() {
+   const currentValue = screen.value;
+   historico.value += currentValue + ' *';
+   screen.value = '';
+ });
